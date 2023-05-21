06887d903b36f60488233be1a45c710fc7f7ceef
Mason Wang <bigmasonwang@gmail.com>
2023-05-21 14:48:35 +0930

    style

--- src/templates/default.jsx
+++ src/templates/default.jsx
@@ -2,7 +2,7 @@ const Default = ({ title, content }) => (
   <div className="flex h-full flex-col justify-center bg-orange-100 p-6">
     <div>
       <div className="text-center text-4xl">{title}</div>
-      <div className="mt-3 text-xl">{content}</div>
+      <div className="mt-3 text-center text-xl">{content}</div>
     </div>
   </div>
 );
