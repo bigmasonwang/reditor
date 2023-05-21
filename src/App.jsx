5a4eff4dcc0b5a25e816c7c5ea0ad75d63cecf07
Mason Wang <bigmasonwang@gmail.com>
2023-05-21 14:44:16 +0930

    add basic template

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import { useForm } from 'react-hook-form';
+import Default from './templates/default';
 
 const App = () => {
   const { register, watch } = useForm({
     defaultValues: {
-      title: '',
-      content: '',
+      title: 'Lorem Ipsum Dolor',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae numquam inventore corrupti velit ipsam.',
+      template: 'Default',
     },
   });
 
-  console.log(watch());
+  const templateOptions = [
+    {
+      name: 'Default',
+      component: Default,
+    },
+  ];
+  const selectedTemplate = templateOptions.find(
+    (option) => option.name === watch('template')
+  )?.component;
+
+  console.log('render');
 
   return (
     <div className="container mx-auto">
-      <form className="grid grid-cols-3 gap-4">
+      <form className="grid grid-cols-2 gap-4">
         <div className="col-span-1">
           <div>
             <label htmlFor="title">Title:</label>
@@ -33,8 +46,34 @@ const App = () => {
               {...register('content')}
             />
           </div>
+          <div>
+            <label htmlFor="template-select">Template:</label>
+            <select
+              id="template-select"
+              className="form-control"
+              {...register('template')}
+            >
+              <option disabled value="">
+                Choose a template
+              </option>
+              {templateOptions.map(({ name }) => (
+                <option value={name} key={name}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div className="col-span-2">asdf</div>
+
+        <div className="col-span-1">
+          <div className="aspect-[3/4] w-full bg-slate-50 p-3">
+            {selectedTemplate &&
+              selectedTemplate({
+                title: watch('title'),
+                content: watch('content'),
+              })}
+          </div>
+        </div>
       </form>
     </div>
   );
